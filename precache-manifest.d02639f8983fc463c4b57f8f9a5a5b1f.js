c84f05f9acb44407d1ddc6ce42a301e0ad015a66
Eitan Elnekave <36443745+eitan02581@users.noreply.github.com>
2020-06-14 21:59:33 +0300

    Initial dist subtree commit

---
+++ precache-manifest.d02639f8983fc463c4b57f8f9a5a5b1f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11446dece178407f525",
+    "url": "/myShop/css/BoughtItems.62bd190e.css"
+  },
+  {
+    "revision": "8c22d601e7cc013e765b",
+    "url": "/myShop/css/ReceivedList.dd63e3c9.css"
+  },
+  {
+    "revision": "843e6636214f2621acee",
+    "url": "/myShop/css/app.7205ae08.css"
+  },
+  {
+    "revision": "4864cb5909aeebebe8c7",
+    "url": "/myShop/css/chunk-vendors.736fe5c4.css"
+  },
+  {
+    "revision": "502e0112ef309e871ce7fac925722d2a",
+    "url": "/myShop/index.html"
+  },
+  {
+    "revision": "f11446dece178407f525",
+    "url": "/myShop/js/BoughtItems.6734c21d.js"
+  },
+  {
+    "revision": "8c22d601e7cc013e765b",
+    "url": "/myShop/js/ReceivedList.4ff056fd.js"
+  },
+  {
+    "revision": "843e6636214f2621acee",
+    "url": "/myShop/js/app.a334880d.js"
+  },
+  {
+    "revision": "4864cb5909aeebebe8c7",
+    "url": "/myShop/js/chunk-vendors.5a360b53.js"
+  },
+  {
+    "revision": "22291f2f0ff0861931a7a17915e88c36",
+    "url": "/myShop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/myShop/robots.txt"
+  }
+]);
